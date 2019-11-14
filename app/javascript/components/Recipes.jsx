96ddcbbb8f5358561ca1e755f9fe5dfbd5c08f9c
Olivia <oliviamcgoffin@Olivias-MacBook-Pro.local>
2019-11-14 10:49:21 -0800

    Minor updates to styling

--- app/javascript/components/Recipes.jsx
+++ app/javascript/components/Recipes.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default () => (
-  <div className="hero bg-primary-color">
-    <h1 className="primary-color">Listing all Recipes</h1>
+  <div className="bg-primary-color text-center">
+    <div className="container bg-secondary-color">
+      <h1 className="primary-color">Listing all Recipes</h1>
+    </div>
   </div>
 );
